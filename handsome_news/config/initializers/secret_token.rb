f339dc8a6b3db49b39b0225dd43739ae70ad0261
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-24 14:19:41 -0500

    first commit

---
+++ handsome_news/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HandsomeNews::Application.config.secret_token = 'fcd4e24220135f4a5bac1f45f5d3fccd9126443b9614315456086b7bc30c7ce8685adb314ec8f5606117bae7d6a232603dcf30c3874426ec74cdeff7caac15e3'
