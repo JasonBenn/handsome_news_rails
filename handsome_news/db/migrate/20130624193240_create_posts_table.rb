1113732c722f27429061346a0ecdf9e0ddc2c27d
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-24 14:45:36 -0500

    copy models and migrations

---
+++ handsome_news/db/migrate/20130624193240_create_posts_table.rb
@@ -0,0 +1,9 @@
+class CreatePostsTable < ActiveRecord::Migration
+  def change
+    create_table :posts do |p|
+      p.string :title
+      p.string :link_url
+      p.integer :user_id
+    end
+  end
+end
