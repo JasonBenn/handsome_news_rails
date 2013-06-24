676eddce6d858991e889972cc9b7f63495a99f55
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-24 14:54:14 -0500

    removed unecessary comment

--- handsome_news/app/models/vote.rb
+++ handsome_news/app/models/vote.rb
@@ -4,8 +4,6 @@ class Vote < ActiveRecord::Base
 
   validates_uniqueness_of :post_id, scope: :user_id
   validates_inclusion_of :value, in: [1, -1]
-  #validate :ensure_not_author
-
 
   def ensure_not_author
     errors.add :user_id, "is the creator of this post" if post.user_id = user_id
