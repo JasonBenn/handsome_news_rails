25513bfb5d6a48d3d043f733246483d59fcf4f0d
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-24 19:06:40 -0500

    Post#create

--- handsome_news/app/controllers/comments_controller.rb
+++ handsome_news/app/controllers/comments_controller.rb
@@ -1,18 +1,20 @@
 class CommentsController < ApplicationController 
 
   def index
-    @post = Post.find(params[:post_id])
-    @comments = @post.comments.all
+    post = Post.find(params[:post_id])
+    @comments = post.comments.all
   end
 
   def new
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.new
+    post = Post.find(params[:post_id])
+    @comment = post.comments.new
   end
 
   def create
     post = Post.find(params[:post_id])
-    comment = post.comments.create(params[:comment])
+    comment = current_user.comments.build(params[:comment])
+    post.comments << comment
+    post.save
     redirect_to post_comments_path(post)
   end
 end
